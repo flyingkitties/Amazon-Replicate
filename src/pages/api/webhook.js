3a45175cac0f6eceb6753259fb443051304a0b36
flyingkitties <110839612+flyingkitties@users.noreply.github.com>
2024-03-13 15:58:32 +0000

    b/ db orders

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -1,9 +1,9 @@
-import { buffer } from "micro";
-import * as admin from "firebase-admin";
-import { getFirestore, Timestamp, FieldValue } from "firebase-admin/firestore";
+import { buffer } from 'micro';
+import * as admin from 'firebase-admin';
+import { getFirestore, Timestamp, FieldValue } from 'firebase-admin/firestore';
 
 //Secure a connection to firebase from the backend
-const serviceAccount = require("../../../permissions.json");
+const serviceAccount = require('../../../permissions.json');
 
 const app = !admin.apps.length
   ? admin.initializeApp({
@@ -13,18 +13,18 @@ const app = !admin.apps.length
   : admin.app();
 
 //Establish connection to stripe
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const endpoitSecret = process.env.STRIPE_SIGNING_SECRET;
 
 const fulfillOrder = async (session) => {
-  console.log("Fulfilling order", session);
+  console.log('Fulfilling order', session);
 
   return app
     .firestore()
-    .collection("users")
+    .collection('users')
     .doc(`session.matadata.email`)
-    .collection("order")
+    .collection('order')
     .doc(session.id)
     .set({
       amount: session.amount_total / 100,
@@ -36,25 +36,23 @@ const fulfillOrder = async (session) => {
       console.log(`Success: order ${session.id} has been added to the BD`);
     })
     .catch((err) => {
-      console.log("Firebase error", err);
+      console.log('Firebase error', err);
     });
 };
 
 export default async (req, res) => {
-  // if (req.method === "GET") {
-  //   const data = await
-  //   app.firestore().collection("test")
-  //   .get();
-  //   const responseContent = data.docs.map((doc) => doc.data());
-  //   console.log({responseContent});
-  //   return res.status(200).send({responseContent});
-  // }
-  if (req.method === "POST") {
-    console.log("inside Post");
+  if (req.method === 'GET') {
+    const data = await app.firestore().collection('test').get();
+    const responseContent = data.docs.map((doc) => doc.data());
+    console.log({ responseContent });
+    return res.status(200).send({ responseContent });
+  }
+  if (req.method === 'POST') {
+    console.log('inside Post');
 
     const requestBuffer = await buffer(req);
     const payload = requestBuffer.toString();
-    const sig = req.headers["stripe-signature"];
+    const sig = req.headers['stripe-signature'];
 
     let event;
 
@@ -62,19 +60,19 @@ export default async (req, res) => {
     try {
       event = stripe.webhooks.constructEvent(payload, sig, endpoitSecret);
     } catch (err) {
-      console.log("ERROR", err.message);
+      console.log('ERROR', err.message);
       return res.status(400).send(`Webhook error: ${err.message}`);
     }
-    console.log("from stripe only");
+    console.log('from stripe only');
     // Handle the checkout session compleated event
-    if (event.type === "checkout.session.completed") {
+    if (event.type === 'checkout.session.completed') {
       const session = event.data.object;
 
-      console.log("fulfilling orders");
+      console.log('fulfilling orders');
       // Fulfill the order
       return fulfillOrder(session)
         .then(() => res.status(200))
-        .catch((err) => console.log("error in fulfilment", err));
+        .catch((err) => console.log('error in fulfilment', err));
     }
   }
 };
--- src/pages/orders.js
+++ src/pages/orders.js
@@ -6,48 +6,6 @@ import moment from 'moment';
 
 import Order from '../components/Order';
 
-function orders({ orders }) {
-  const { data: session, status } = useSession();
-
-  console.log(db.orders);
-
-  return (
-    <div>
-      <Header />
-
-      <main className="max-w-screen-lg mx-auto p-10 bg-gray-100 ">
-        <h1 className="text-3xl border-b mb-2 pb-1 border-yellow-400">
-          Your orders{' '}
-        </h1>
-
-        {session ? (
-          <h2>{orders.length} orders </h2>
-        ) : (
-          <h2>Please sign in to see your orders</h2>
-        )}
-
-        <div className="mt-5 space-y-4">
-          {orders?.map(
-            ({ id, amount, amountShipping, items, timestamp, images }) => (
-              <Order
-                key={id}
-                id={id}
-                amount={amount}
-                amountShipping={amountShipping}
-                items={items}
-                timestamp={timestamp}
-                images={images}
-              />
-            ),
-          )}
-        </div>
-      </main>
-    </div>
-  );
-}
-
-export default orders;
-
 export async function getServerSideProps(context) {
   const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
@@ -92,3 +50,43 @@ export async function getServerSideProps(context) {
     },
   };
 }
+
+function orders({ orders }) {
+  const { data: session, status } = useSession();
+
+  return (
+    <div>
+      <Header />
+
+      <main className="max-w-screen-lg mx-auto p-10 bg-gray-100 ">
+        <h1 className="text-3xl border-b mb-2 pb-1 border-yellow-400">
+          Your orders{' '}
+        </h1>
+
+        {session ? (
+          <h2>{orders.length} orders </h2>
+        ) : (
+          <h2>Please sign in to see your orders</h2>
+        )}
+
+        <div className="mt-5 space-y-4">
+          {orders?.map(
+            ({ id, amount, amountShipping, items, timestamp, images }) => (
+              <Order
+                key={id}
+                id={id}
+                amount={amount}
+                amountShipping={amountShipping}
+                items={items}
+                timestamp={timestamp}
+                images={images}
+              />
+            ),
+          )}
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default orders;
