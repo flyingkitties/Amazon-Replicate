a2d794e3c83c4cfd7d83f9074589f366a25b1be0
flyingkitties <110839612+flyingkitties@users.noreply.github.com>
2025-05-30 11:44:54 +0100

    Fix 2 on firebase auth

--- Firebase.js
+++ Firebase.js
@@ -1,10 +1,6 @@
-// Import the functions you need from the SDKs
 import { initializeApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-import 'firebase/compat/auth';
-import firebase from 'firebase/compat/app';
-//  import * as admin from "firebase-admin";
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -16,16 +12,22 @@ const firebaseConfig = {
   appId: '1:33219224981:web:a409f9240c2b0b713e829c',
 };
 
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
 // Initialize Firebase
 
 // const app = initializeApp(firebaseConfig);
 
-const app = !firebase.apps.length
-  ? firebase.initializeApp(firebaseConfig)
-  : firebase.app();
-const db = app.firestore();
+// const app = !firebase.apps.length
+//   ? firebase.initializeApp(firebaseConfig)
+//   : firebase.app();
+// const db = app.firestore();
 
-export default db;
+// export default db;
 // export const auth = getAuth(app);
 
 // export const auth = getAuth(app)
